8db99603cd6d02aebf31196e5b59cfd464d23a71
v2an <romanv2an@gmail.com>
2021-08-07 09:39:30 +0300

    HW13

--- Секция3/ДЗ 13/HW13/HW13.cpp
+++ Секция3/ДЗ 13/HW13/HW13.cpp
@@ -1,20 +1,187 @@
-﻿// HW13.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+using namespace std;
 
-#include <iostream>
+struct StackNode
+{
+    int dat;
+    StackNode* next;
+};
+
+struct Stack
+{
+    int size;
+    StackNode* head;
+};
+
+void init(Stack* st)
+{
+    st->size = 0;
+    st->head = nullptr;
+}
+
+void pushST(Stack* st, int dat)
+{
+    StackNode* tmp = new StackNode;
+    if (tmp == nullptr)
+    {
+        cout << "Stack overflow" << endl;
+        return;
+    }
+    tmp->dat = dat;
+    tmp->next = st->head;
+    st->head = tmp;
+    st->size++;
+    return;
+}
+
+void popST(Stack* st)
+{
+    if (st->size == 0)
+    {
+        cout << "Stack is empty" << endl;
+        return;
+    }
+    StackNode* tmp = st->head;
+    int data = st->head->dat;
+    st->head = st->head->next;
+    delete tmp;
+    st->size--;
+    return;
+}
+
+const int n = 6;
+int matrix[n][n] = {
+    {0, 1, 1, 0, 0, 0},
+    {0, 0, 0, 1, 1, 1},
+    {0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 1, 0},
+};
+int visited[n] = { 0 };
+
+void depthTraversStack(Stack* st, int start)
+{
+    int r;
+    int current = start;
+    pushST(st, start);
+    cout << start << " ";
+    visited[start] = 1;
+    while (st->size != 0)
+    {
+        int check = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (matrix[st->head->dat][i] == 1 && visited[i] == 0)
+            {
+                pushST(st, i);
+                visited[i] = 1;
+                //cout << st->head << endl;
+                cout << i << " ";
+                check = 1;
+                break;
+            }
+
+        }
+        if (check == 0)
+        {
+            popST(st);
+        }
+        //cout << st->size << endl;
+    }
+}
+
+// ================================================================== EXRS 2 ================================
+const int n2 = 6;
+
+int matrix2[n2][n2] = {
+    {0, 1, 1, 0, 0, 1},
+    {1, 0, 0, 0, 0, 1},
+    {0, 0, 0, 0, 0, 1},
+    {0, 0, 0, 0, 1, 0},
+    {0, 0, 0, 1, 0, 0},
+    {0, 1, 0, 0, 0, 0},
+};
+
+int visitedRecurse[n2] = { 0 };
+int visitedRecurseCount[n2] = { 0 };
+int visitedMatrix[n2] = { 0 };
+
+void CountCheckMatrix()
+{
+    for (int i = 0; i < n2; i++)
+    {
+        for (int j = 0; j < n2; j++)
+        {
+            if ((matrix2[i][j] == 1) && (i != j))
+            {
+                visitedMatrix[i]++;
+            }
+        }
+    }
+}
+
+void resetArr()
+{
+    for (int i = 0; i < n; i++)
+    {
+        visitedRecurse[i] = 0;
+    }
+}
+
+void CountCheckRecurse(int st)
+{
+    int i;
+    visitedRecurse[st] = 1;
+    for (i = 0; i < n2; i++)
+    {
+        if ((i != st) && matrix2[st][i] == 1)
+        {
+            visitedRecurseCount[st]++;
+        }
+        if (matrix2[st][i] == 1 && !visitedRecurse[i])
+        {
+            CountCheckRecurse(i);
+        }
+    } 
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    // ============================================================== EXRS 1 ================================
+    cout << "================================ EXRS 1 ================================" << endl << endl;
+    Stack* stack1 = new Stack;
+    init(stack1);
+    depthTraversStack(stack1, 2);
+    /*
+    for (int i = 0; i < n; i++)
+    {
+        if (visited[i] == 1)
+            cout << i << " ";
+    }
+    */
+    cout << endl;
+    // ============================================================== EXRS 2 ================================
+    cout << endl << "================================ EXRS 2 ================================" << endl << endl;
+    int st = 1;
+    CountCheckMatrix();
+    for (int i = 0; i < n2; i++)
+    {
+        cout << (i+1) << " | ";
+    }
+    cout << endl << "Matrix output" << endl;
+    for (int i = 0; i < n2; i++)
+    {
+        cout << visitedMatrix[i] << " | ";
+    }
+    cout << endl;
+    CountCheckRecurse(st);
+    resetArr();
+    cout << "Recurse output" <<endl;
+    for (int i = 0; i < n2; i++)
+    {
+        cout << visitedRecurseCount[i] << " | ";
+    }
+
+
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
