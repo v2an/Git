5bf1776aca28e988180f319aaf5b8998fa598679
v2an <romanv2an@gmail.com>
2021-08-08 19:11:22 +0300

    HW 15 cleared

--- Секция3/ДЗ 15/HW15/HW15.cpp
+++ Секция3/ДЗ 15/HW15/HW15.cpp
@@ -16,8 +16,45 @@ int EX1HashCode(string input)
 }
 
 // ====================================================== EXRS 2 ======================================================
-int HungryAlg(int* coins, int target, int summ, int last, int count)
+
+struct Coin
 {
+    int value;
+    Coin* next;
+};
+
+struct list
+{
+    int size;
+    Coin* head;
+};
+
+void listinit(list* list)
+{
+    list->head = nullptr;
+    list->size = 0;
+}
+
+void push(list* st, int dat)
+{
+    Coin* tmp = new Coin;
+    if (tmp == nullptr)
+    {
+        std::cout << "Stack overflow" << endl;
+        return;
+    }
+    tmp->value = dat;
+    tmp->next = st->head;
+    st->head = tmp;
+    st->size++;
+    return;
+}
+
+int HungryAlg(int* coins, int target, list* COINS)
+{
+    int last = target;
+    int summ = 0;
+    int count = 0;
     while (summ < target)
     {
         for (int i = 0; i < 5; i++)
@@ -27,6 +64,7 @@ int HungryAlg(int* coins, int target, int summ, int last, int count)
                 summ = summ + coins[i];
                 last = last - coins[i];
                 count++;
+                push(COINS, coins[i]);
                 break;
             }
         }
@@ -35,25 +73,39 @@ int HungryAlg(int* coins, int target, int summ, int last, int count)
     return count;
 }
 
+void printStack(list* st)
+{
+    Coin* tmp = st->head;
+    int Q = st->size;
+    for (int i = 0; i < Q; i++)
+    {
+        cout << "[" << tmp->value << "] ";
+        tmp = tmp->next;
+    }
+    cout << endl;
+}
 
 int main()
 {
     // ====================================================== EXRS 1 ======================================================
 
-    cout << "=================================================== EXRS 1 ===================================================" << endl;
+    cout << "=================================================== EXRS 1 ===================================================" << endl << endl;
     cout << "Enter somthing" << endl;
     string input;
     cin >> input;
-    cout << "HashCode: " << EX1HashCode(input) << endl;
+    cout << "HashCode: " << EX1HashCode(input) << endl << endl;
 
     // ====================================================== EXRS 1 ======================================================
 
-    cout << "=================================================== EXRS 2 ===================================================" << endl;
+    cout << "=================================================== EXRS 2 ===================================================" << endl << endl;
     int coins[5] = { 50, 10, 5, 2, 1 };
-    int target = 98;
-    int last = target;
-    int summ = 0;
-    int count = 0;
-    cout << HungryAlg(coins, target, summ, last, count);
+    cout << "Enter target summ: ";
+    int target;
+    cin >> target;
+    list* COINS = new list;
+    listinit(COINS);
+    cout << endl << "Nomber of coins: " << HungryAlg(coins, target, COINS) << endl;
+    cout << "Coins: ";
+    printStack(COINS);
     return 0;
 }
