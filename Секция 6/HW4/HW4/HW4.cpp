f5657cfc8631c4f8362e250330aa7a831eb72056
v2an <romanv2an@gmail.com>
2021-12-06 20:07:11 +0300

    HW4 ended

--- Секция 6/HW4/HW4/HW4.cpp
+++ Секция 6/HW4/HW4/HW4.cpp
@@ -10,14 +10,13 @@ void insert_sorted(std::vector<int>& untrvector, int i)
     std::vector<int>::iterator it = std::lower_bound(begin(untrvector), end(untrvector), i);
     untrvector.insert(it, i);
 }
-/*
-template< typename  inp>
-void insert_sorted_temp(std::vector<inp>& untrvector, inp i)
+
+template< typename  container, typename  input>
+void insert_sorted_temp(container& untrvector, input i)
 {
-    std::vector<inp>::iterator it = std::lower_bound(begin(untrvector), end(untrvector), i);
-    untrvector.insert(it, i);
+    untrvector.insert((std::lower_bound(begin(untrvector), end(untrvector), i)), i);
 }
-*/
+
 std::vector<double> operator-(const std::vector<double>& a, const std::vector<int>& b)
 {
     std::vector<double> c(a.size());
@@ -33,15 +32,17 @@ int main()
     std::vector<int> exrsz1(30);
     std::generate(exrsz1.begin(), exrsz1.end(), []() {return (std::rand() % 57); });
     std::sort(exrsz1.begin(), exrsz1.end());
+    std::cout << "Vector before insert:\t";
     std::for_each(exrsz1.begin(), exrsz1.end(), [](int i) { std::cout << i << " "; });
     std::cout << std::endl;
     insert_sorted(exrsz1, 30);
+    std::cout << "1'st insert:\t\t";
+    std::for_each(exrsz1.begin(), exrsz1.end(), [](int i) { std::cout << i << " "; });
+    std::cout << std::endl;
+    insert_sorted_temp(exrsz1, 27);
+    std::cout << "2'nd insert:\t\t";
     std::for_each(exrsz1.begin(), exrsz1.end(), [](int i) { std::cout << i << " "; });
     std::cout << std::endl << std::endl;
-
-    //insert_sorted_temp(exrsz1, 27);
-    //std::for_each(exrsz1.begin(), exrsz1.end(), [](int i) { std::cout << i << " "; });
-    //std::cout << std::endl << std::endl;
 
 
     // ======================= 2 =========================
@@ -55,7 +56,7 @@ int main()
     std::copy(analog.begin(), analog.end(), digital.begin());
     //std::for_each(digital.begin(), digital.end(), [](int i) { std::cout << i << " "; });
     std::vector<double> error = analog - digital;
-    std::for_each(error.begin(), error.end(), [](double i) { std::cout << i << " "; });
+    //std::for_each(error.begin(), error.end(), [](double i) { std::cout << i << " "; });
     //std::cout << std::endl;
     transform(error.begin(), error.end(), error.begin(), [](double i)
         {
